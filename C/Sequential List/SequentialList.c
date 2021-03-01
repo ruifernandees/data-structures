ae91c02e8dd8f018cf8be53250fd01b1e50bfd0e
ruifernandees <dev.ruifernandes@gmail.com>
2021-03-01 16:03:08 -0300

    Change SequentialList organization (folder)

--- C/Sequential List/SequentialList.c
+++ C/Sequential List/SequentialList.c
@@ -12,49 +12,7 @@ typedef struct {
     int numberOfElements;
 } LIST;
 
-int main() {
-    void initStructure(LIST* list);
-    int getSize(LIST* list);
-    void showList(LIST* list);
-    int sequentialSearch(LIST* list, TYPEKEY key);
-    int sentinelSearch(LIST* list, TYPEKEY key);
-    int insertListElement(LIST* list, REGISTER reg, int index);
-    int removeListElement(LIST* list, TYPEKEY key);
-    void rebootList(LIST* list);
 
-    LIST myList;
-    initStructure(&myList);
-
-    REGISTER register1 = { 10 };
-    REGISTER register2 = { 15 };
-    REGISTER register3 = { 20 };
-
-    insertListElement(&myList, register1, 0);
-    insertListElement(&myList, register2, 0);
-    insertListElement(&myList, register3, 0);
-
-    showList(&myList);
-    printf("Size: %d\n\n", getSize(&myList));
-
-    printf("10 is %d\n", sentinelSearch(&myList, 10));
-    printf("15 is %d\n", sentinelSearch(&myList, 15));
-    printf("20 is %d\n\n\n", sentinelSearch(&myList, 20));
-
-    removeListElement(&myList, 10);
-
-    showList(&myList);
-    printf("Size: %d\n\n", getSize(&myList));
-
-    printf("10 is %d\n", sentinelSearch(&myList, 10));
-    printf("20 is %d\n\n\n", sentinelSearch(&myList, 20));
-
-    rebootList(&myList);
-
-    showList(&myList);
-    printf("Size: %d\n\n", getSize(&myList));
-    
-    return 0;
-}
 
 void initStructure(LIST* list) {
     list->numberOfElements = 0;
@@ -121,6 +79,7 @@ int insertListElement(LIST* list, REGISTER reg, int index) {
     list->numberOfElements++;
     return 1;
 }
+
 
 int removeListElement(LIST* list, TYPEKEY key) {
 
---
+++ C/Sequential List/ex01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "SequentialList.c"
+
+int main() {
+    LIST myList;
+    initStructure(&myList);
+
+    REGISTER register1 = { 10 };
+    REGISTER register2 = { 15 };
+    REGISTER register3 = { 20 };
+
+    insertListElement(&myList, register1, 0);
+    insertListElement(&myList, register2, 0);
+    insertListElement(&myList, register3, 0);
+
+    showList(&myList);
+    printf("Size: %d\n\n", getSize(&myList));
+
+    printf("10 is %d\n", sequentialSearch(&myList, 10));
+    printf("15 is %d\n", sequentialSearch(&myList, 15));
+    printf("20 is %d\n\n\n", sequentialSearch(&myList, 20));
+
+    removeListElement(&myList, 10);
+
+    showList(&myList);
+    printf("Size: %d\n\n", getSize(&myList));
+
+    printf("10 is %d\n", sequentialSearch(&myList, 10));
+    printf("20 is %d\n\n\n", sequentialSearch(&myList, 20));
+
+    rebootList(&myList);
+
+    showList(&myList);
+    printf("Size: %d\n\n", getSize(&myList));
+    
+    return 0;
+}

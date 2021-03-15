f9de9dbbcd0ae63c5d0168052ad4eda803d302b7
ruifernandees <dev.ruifernandes@gmail.com>
2021-03-15 13:28:13 -0300

    Add Stack example: decimalToBinary

---
+++ C/ADTs/Stack (Static)/decimalToBinary.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main()
+{
+    int number;
+    printf("Type a number: ");
+    scanf("%d", &number);
+
+    
+    tStack *binaries;
+
+    binaries = createStack();
+
+    int currentNumber = number;
+
+    while (currentNumber > 0)
+    {
+        int quotient = currentNumber / 2;
+        int rest = currentNumber % 2;
+
+        push(binaries, rest);
+
+        currentNumber = quotient;
+    }
+
+    printf("%d in binary is: ", number);
+    while (!isEmpty(binaries))
+    {
+        int currentBit;
+        pop(binaries, &currentBit);
+        printf("%d", currentBit);
+    }
+    printf("\n"); 
+
+    
+    return 0;
+}
